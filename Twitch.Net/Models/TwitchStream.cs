7ae2e29a8665d45454035ba3aa7f957fd4ef2f9d
Cryma <crymaprivat@gmail.com>
2019-11-08 18:32:51 +0100

    Add twitch stream response model

---
+++ Twitch.Net/Models/TwitchStream.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Models
+{
+    public class TwitchStream
+    {
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
+
+        [JsonPropertyName("user_name")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("game_id")]
+        public string GameId { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("viewer_count")]
+        public int ViewerCount { get; set; }
+
+        [JsonPropertyName("started_at")]
+        public string StartedAt { get; set; }
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+
+        [JsonPropertyName("thumbnail_url")]
+        public string ThumbnailUrl { get; set; }
+
+    }
+}
