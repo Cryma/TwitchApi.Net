364784e5ee3652aa89956f6294ad8d668e4f48b6
Cryma <crymaprivat@gmail.com>
2019-11-08 16:42:36 +0100

    Add GetUsers api endpoints

---
+++ Twitch.Net/TwitchApi.Users.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Twitch.Net.Models;
+using Twitch.Net.Response;
+
+namespace Twitch.Net
+{
+    public partial class TwitchApi
+    {
+
+        private const string _getUsersEndpoint = "https://api.twitch.tv/helix/users";
+
+        public async Task<TwitchUser> GetUser(string id)
+        {
+            using var httpClient = GetPreparedHttpClient();
+
+            var url = $"{_getUsersEndpoint}?{GetQueryForParameters("id", new[] {id})}";
+            var response = await httpClient.GetAsync(url);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return null;
+            }
+
+            var deserializedObject =  await JsonSerializer.DeserializeAsync<TwitchResponse<TwitchUser>>(await response.Content.ReadAsStreamAsync());
+
+            return deserializedObject.Data.FirstOrDefault();
+        }
+
+        public async Task<TwitchUser[]> GetUsers(string[] ids)
+        {
+            using var httpClient = GetPreparedHttpClient();
+
+            var url = $"{_getUsersEndpoint}?{GetQueryForParameters("id", ids)}";
+            var response = await httpClient.GetAsync(url);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return null;
+            }
+
+            var deserializedObject = await JsonSerializer.DeserializeAsync<TwitchResponse<TwitchUser>>(await response.Content.ReadAsStreamAsync());
+
+            return deserializedObject.Data;
+        }
+
+    }
+}
--- Twitch.Net/TwitchApi.cs
+++ Twitch.Net/TwitchApi.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 
 namespace Twitch.Net
 {
-    public class TwitchApi
+    public partial class TwitchApi
     {
 
         private readonly string _clientId;
@@ -11,6 +13,22 @@ namespace Twitch.Net
         public TwitchApi(string clientId)
         {
             _clientId = clientId;
+        }
+
+        private HttpClient GetPreparedHttpClient()
+        {
+            return new HttpClient
+            {
+                DefaultRequestHeaders =
+                {
+                    { "Client-ID", _clientId }
+                }
+            };
+        }
+
+        private string GetQueryForParameters(string parameterName, IEnumerable<string> parameters)
+        {
+            return string.Join("&", parameters.Select(x => $"{parameterName}={x}"));
         }
 
     }
