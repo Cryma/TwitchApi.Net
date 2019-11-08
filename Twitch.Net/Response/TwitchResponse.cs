73b67096c26b222b346050dc0126068b32723fef
Cryma <crymaprivat@gmail.com>
2019-11-08 16:23:28 +0100

    Add generic twitch response

---
+++ Twitch.Net/Response/TwitchResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Response
+{
+    public class TwitchResponse<TResponseObject>
+    {
+        
+        [JsonPropertyName("data")]
+        public TResponseObject[] Data { get; set; }
+
+    }
+}
