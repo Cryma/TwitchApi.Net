c54b1339241e1f45bf077096d072161a5685ffed
Cryma <crymaprivat@gmail.com>
2019-11-08 18:06:30 +0100

    Add generic paginated twitch response

---
+++ Twitch.Net/Response/TwitchPaginatedResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using Twitch.Net.Models;
+
+namespace Twitch.Net.Response
+{
+    public class TwitchPaginatedResponse<TResponseObject>
+    {
+
+        [JsonPropertyName("data")]
+        public TResponseObject[] Data { get; set; }
+
+        [JsonPropertyName("pagination")]
+        public TwitchPagination Pagination { get; set; }
+
+    }
+}
