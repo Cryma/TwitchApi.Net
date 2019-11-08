43c35c4202bfdb64e9d7c233676529d81ad9ae5b
Cryma <crymaprivat@gmail.com>
2019-11-08 18:06:19 +0100

    Add twitch game response model

---
+++ Twitch.Net/Models/TwitchGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Models
+{
+    public class TwitchGame
+    {
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("box_art_url")]
+        public string BoxArtUrl { get; set; }
+
+    }
+}
