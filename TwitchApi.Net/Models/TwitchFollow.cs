20d8332adb9ed8c9ccf70fdf578550fe560fa376
Cryma <crymaprivat@gmail.com>
2019-11-09 16:49:01 +0100

    Add twitch follow response model

---
+++ TwitchApi.Net/Models/TwitchFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TwitchApi.Net.Models
+{
+    public class TwitchFollow
+    {
+
+        [JsonPropertyName("from_id")]
+        public string FromId { get; set; }
+
+        [JsonPropertyName("from_name")]
+        public string FromName { get; set; }
+
+        [JsonPropertyName("to_id")]
+        public string ToId { get; set; }
+
+        [JsonPropertyName("to_name")]
+        public string ToName { get;set; }
+
+        [JsonPropertyName("followed_at")]
+        public string FollowedAt { get; set; }
+
+    }
+}
