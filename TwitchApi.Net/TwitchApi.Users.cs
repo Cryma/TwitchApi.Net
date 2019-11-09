19a239521e6824b22ce1830a933e90b1ef1541f0
Cryma <crymaprivat@gmail.com>
2019-11-09 16:49:27 +0100

    Add GetUsersFollows endpoint

--- TwitchApi.Net/TwitchApi.Users.cs
+++ TwitchApi.Net/TwitchApi.Users.cs
@@ -10,6 +10,7 @@ namespace TwitchApi.Net
     {
 
         private const string _getUsersEndpoint = "https://api.twitch.tv/helix/users";
+        private const string _getUsersFollowsEndpoint = "https://api.twitch.tv/helix/users/follows";
 
         public async Task<TwitchResponse<TwitchUser>> GetUsers(string[] ids)
         {
@@ -19,6 +20,32 @@ namespace TwitchApi.Net
             var responseStream = await httpClient.GetAsync(url, _clientId);
 
             return await JsonSerializer.DeserializeAsync<TwitchResponse<TwitchUser>>(responseStream);
+        }
+
+        public async Task<TwitchPaginatedResponseWithTotal<TwitchFollow>> GetUsersFollows(int first = 20, string after = null, string toId = null, string fromId = null)
+        {
+            using var httpClient = GetHttpClient();
+
+            var url = $"{_getUsersFollowsEndpoint}?first={first}";
+
+            if(string.IsNullOrEmpty(after) == false)
+            {
+                url += $"&after={after}";
+            }
+
+            if (string.IsNullOrEmpty(toId) == false)
+            {
+                url += $"&to_id={toId}";
+            }
+
+            if (string.IsNullOrEmpty(fromId) == false)
+            {
+                url += $"&from_id={fromId}";
+            }
+
+            var responseStream = await httpClient.GetAsync(url, _clientId);
+
+            return await JsonSerializer.DeserializeAsync<TwitchPaginatedResponseWithTotal<TwitchFollow>>(responseStream);
         }
 
     }
