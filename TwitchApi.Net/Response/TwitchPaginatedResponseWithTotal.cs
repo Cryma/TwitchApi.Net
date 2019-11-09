640645e3b0c5ac11a3552920e8f7d95fdc1447fd
Cryma <crymaprivat@gmail.com>
2019-11-09 16:49:13 +0100

    Add generic twitch paginated response model with total amount

---
+++ TwitchApi.Net/Response/TwitchPaginatedResponseWithTotal.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TwitchApi.Net.Response
+{
+    public class TwitchPaginatedResponseWithTotal<TResponseObject> : TwitchPaginatedResponse<TResponseObject>
+    {
+
+        [JsonPropertyName("total")]
+        public int Total { get; set; }
+
+    }
+}
