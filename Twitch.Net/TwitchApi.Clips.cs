1a5dc70ebb30e5ade4cef1185d3cd88654abbea0
Cryma <crymaprivat@gmail.com>
2019-11-09 18:42:25 +0100

    Add GetClips api endpoint

---
+++ Twitch.Net/TwitchApi.Clips.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Twitch.Net.Models;
+using Twitch.Net.Response;
+
+namespace Twitch.Net
+{
+    public partial class TwitchApi
+    {
+
+        private const string _getClipsEndpoint = "https://api.twitch.tv/helix/clips";
+
+        public async Task<TwitchResponse<TwitchClip>> GetClips(string[] clipIds)
+        {
+            using var httpClient = GetHttpClient();
+
+            var parameters = new List<KeyValuePair<string, string>>();
+            parameters.AddRange(clipIds.Select(clipId => new KeyValuePair<string, string>("id", clipId)));
+
+            var responseStream = await httpClient.GetAsync(_getClipsEndpoint, parameters, _clientId);
+
+            return await JsonSerializer.DeserializeAsync<TwitchResponse<TwitchClip>>(responseStream);
+        }
+
+        public async Task<TwitchPaginatedResponse<TwitchClip>> GetClipsFromGames(string gameId, int first = 20, string after = null, string before = null)
+        {
+            using var httpClient = GetHttpClient();
+
+            var parameters = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("game_id", gameId),
+                new KeyValuePair<string, string>("first", first.ToString())
+            };
+
+            if (string.IsNullOrEmpty(after) == false)
+            {
+                parameters.Add(new KeyValuePair<string, string>("after", after));
+            }
+
+            if (string.IsNullOrEmpty(before) == false)
+            {
+                parameters.Add(new KeyValuePair<string, string>("before", before));
+            }
+
+            var responseStream = await httpClient.GetAsync(_getClipsEndpoint, parameters, _clientId);
+
+            return await JsonSerializer.DeserializeAsync<TwitchPaginatedResponse<TwitchClip>>(responseStream);
+        }
+
+        public async Task<TwitchPaginatedResponse<TwitchClip>> GetClipsFromBroadcaster(string broadcasterId, int first = 20, string after = null, string before = null)
+        {
+            using var httpClient = GetHttpClient();
+
+            var parameters = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("broadcaster_id", broadcasterId),
+                new KeyValuePair<string, string>("first", first.ToString())
+            };
+
+            if (string.IsNullOrEmpty(after) == false)
+            {
+                parameters.Add(new KeyValuePair<string, string>("after", after));
+            }
+
+            if (string.IsNullOrEmpty(before) == false)
+            {
+                parameters.Add(new KeyValuePair<string, string>("before", before));
+            }
+
+            var responseStream = await httpClient.GetAsync(_getClipsEndpoint, parameters, _clientId);
+
+            return await JsonSerializer.DeserializeAsync<TwitchPaginatedResponse<TwitchClip>>(responseStream);
+        }
+
+    }
+}
