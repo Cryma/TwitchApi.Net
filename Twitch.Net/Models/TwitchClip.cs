6af081389490e882f92d85ba0661677484e538e4
Cryma <crymaprivat@gmail.com>
2019-11-09 18:42:10 +0100

    Add twitch clip response model

---
+++ Twitch.Net/Models/TwitchClip.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Models
+{
+    public class TwitchClip
+    {
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("embed_url")]
+        public string EmbedUrl { get; set; }
+
+        [JsonPropertyName("broadcaster_id")]
+        public string BroadcasterId { get; set; }
+
+        [JsonPropertyName("broadcaster_name")]
+        public string BroadcasterName { get; set; }
+
+        [JsonPropertyName("creator_id")]
+        public string CreatorId { get; set; }
+
+        [JsonPropertyName("creator_name")]
+        public string CreatorName { get; set; }
+
+        [JsonPropertyName("video_id")]
+        public string VideoId { get; set; }
+
+        [JsonPropertyName("game_id")]
+        public string GameId { get; set; }
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("view_count")]
+        public int ViewCount { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonPropertyName("thumbnail_url")]
+        public string ThumbnailUrl { get; set; }
+
+    }
+}
