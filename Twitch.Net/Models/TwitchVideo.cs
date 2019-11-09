8299d5641c79e3fc07cb312be77449031d3296ad
Cryma <crymaprivat@gmail.com>
2019-11-09 19:04:53 +0100

    Add twitch video response model

---
+++ Twitch.Net/Models/TwitchVideo.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Models
+{
+    public class TwitchVideo
+    {
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
+
+        [JsonPropertyName("user_name")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonPropertyName("published_at")]
+        public string PublishedAt { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("thumbnail_url")]
+        public string ThumbnailUrl { get; set; }
+
+        [JsonPropertyName("viewable")]
+        public string Viewable { get; set; }
+
+        [JsonPropertyName("view_count")]
+        public int ViewCount { get; set; }
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("duration")]
+        public string Duration { get; set; }
+
+    }
+}
