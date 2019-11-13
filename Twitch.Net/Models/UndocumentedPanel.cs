90582759d0b951259148e5c94273c0a71c22cfe2
Cryma <crymaprivat@gmail.com>
2019-11-13 13:26:01 +0100

    Add undocumented user panel response model

---
+++ Twitch.Net/Models/UndocumentedPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Models
+{
+    public class UndocumentedPanel
+    {
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public int UserId { get; set; }
+
+        [JsonPropertyName("display_order")]
+        public int DisplayOrder { get; set; }
+
+        [JsonPropertyName("kind")]
+        public string Kind { get; set; }
+
+        [JsonPropertyName("data")]
+        public UndocumentedPanelData Data { get; set; }
+
+        [JsonPropertyName("html_description")]
+        public string HtmlDescription { get; set; }
+
+        [JsonPropertyName("channel")]
+        public string Channel { get; set; }
+
+    }
+}
---
+++ Twitch.Net/Models/UndocumentedPanelData.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Models
+{
+    public class UndocumentedPanelData
+    {
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("link")]
+        public string Link { get; set; }
+
+    }
+}
