be2c19ecd941a244d4de84ed3d92f98fb9df5582
Fabian Vowie <crymaprivat@gmail.com>
2020-02-06 23:47:25 +0100

    Add enums for various helix model fields

---
+++ Twitch.Net/Models/Enums/HelixUserType.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Models.Enums
+{
+    /// <summary>
+    /// Helix user type
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumMemberConverter))]
+    public enum HelixUserType
+    {
+        /// <summary>
+        /// Normal user
+        /// </summary>
+        [EnumMember(Value = "")]
+        None,
+
+        /// <summary>
+        /// User is staff
+        /// </summary>
+        [EnumMember(Value = "staff")]
+        Staff,
+
+        /// <summary>
+        /// User is admin
+        ///
+        /// <para>Note: This type does not exist anymore</para>
+        /// </summary>
+        [EnumMember(Value = "admin")]
+        Admin,
+
+        /// <summary>
+        /// User is global moderator
+        ///
+        /// <para>Note: This type does not exist anymore</para>
+        /// </summary>
+        [EnumMember(Value = "global_mod")]
+        GlobalModerator
+    }
+}
---
+++ Twitch.Net/Models/Enums/HelixVideoViewableType.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Twitch.Net.Models.Enums
+{
+    /// <summary>
+    /// Helix video viewable type
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumMemberConverter))]
+    public enum HelixVideoViewableType
+    {
+        /// <summary>
+        /// Video is public
+        /// </summary>
+        [EnumMember(Value = "public")]
+        Public,
+
+        /// <summary>
+        /// Video is private
+        /// </summary>
+        [EnumMember(Value = "private")]
+        Private,
+    }
+}
