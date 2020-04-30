e40df779aa4708cf17b24e1746770d78d2d6edae
Fabian Vowie <crymaprivat@gmail.com>
2020-05-01 01:40:28 +0200

    Add user-supplied access token strategy

---
+++ Twitch.Net/Strategies/AccessTokenSuppliedStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Twitch.Net.Interfaces;
+
+namespace Twitch.Net.Strategies
+{
+    internal class AccessTokenSuppliedStrategy : IAccessTokenStrategy
+    {
+
+        private readonly string _accessToken;
+
+        internal AccessTokenSuppliedStrategy(string accessToken)
+        {
+            _accessToken = accessToken;
+        }
+
+        public Task<string> GetAccessToken()
+        {
+            return Task.FromResult(_accessToken);
+        }
+
+    }
+}
