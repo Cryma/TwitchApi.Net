d51326617b6fc290385b131df53622a85312b1af
Fabian Vowie <crymaprivat@gmail.com>
2020-05-01 13:53:07 +0200

    Add validation to TwitchApiBuilder

--- Twitch.Net/Exceptions/TwitchUnauthorizedException.cs
+++ Twitch.Net/Exceptions/TwitchUnauthorizedException.cs
@@ -4,9 +4,16 @@ namespace Twitch.Net.Exceptions
 {
     public class TwitchUnauthorizedException : Exception
     {
+        public TwitchUnauthorizedException() : base()
+        {
+        }
+
         public TwitchUnauthorizedException(string message) : base(message)
         {
         }
 
+        public TwitchUnauthorizedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
     }
 }
