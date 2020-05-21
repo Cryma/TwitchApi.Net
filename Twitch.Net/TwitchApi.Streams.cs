d2d4e2fa7953d1ca4a7585d7ea6065987fe08e8c
SamIX7 <33102148+SamIX7@users.noreply.github.com>
2020-05-21 22:09:49 +0200

    Update TwitchApi.Streams.cs
    Add parameter support for language and user_login.

--- Twitch.Net/TwitchApi.Streams.cs
+++ Twitch.Net/TwitchApi.Streams.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -15,12 +15,13 @@ namespace Twitch.Net
         /// <summary>
         /// Get streams for specific games
         /// </summary>
-        /// <param name="gameIds">Array of game ids. Limit: 100</param>
+        /// <param name="gameIds">Array of game ids. Limit: 10</param>
         /// <param name="first">Amount of streams. Limit: 100</param>
+        /// <param name="language">Stream language. Limit: 100</param>
         /// <param name="after">Cursor for pagination</param>
         /// <param name="before">Cursor for pagination</param>
         /// <returns><see cref="HelixPaginatedResponse{HelixStream}"/> with streams</returns>
-        public async Task<HelixPaginatedResponse<HelixStream>> GetStreams(string[] gameIds, int first = 20, string after = null, string before = null)
+        public async Task<HelixPaginatedResponse<HelixStream>> GetStreams(string[] gameIds, int first = 20, string language = null, string after = null, string before = null)
         {
             using var httpClient = GetHttpClient();
 
@@ -41,6 +42,11 @@ namespace Twitch.Net
                 parameters.Add(new KeyValuePair<string, string>("before", before));
             }
 
+            if (string.IsNullOrEmpty(language) == false)
+            {
+                parameters.Add(new KeyValuePair<string, string>("language", language));
+            }
+            
             var responseStream = await httpClient.GetAsync(_getStreamsEndpoint, parameters);
 
             return await JsonSerializer.DeserializeAsync<HelixPaginatedResponse<HelixStream>>(responseStream);
@@ -51,10 +57,11 @@ namespace Twitch.Net
         /// </summary>
         /// <param name="userIds">Array of user ids. Limit: 100</param>
         /// <param name="first">Amount of streams. Limit: 100</param>
+		/// <param name="language">Stream language. Limit: 100</param>
         /// <param name="after">Cursor for pagination</param>
         /// <param name="before">Cursor for pagination</param>
         /// <returns><see cref="HelixPaginatedResponse{HelixStream}"/> with streams</returns>
-        public async Task<HelixPaginatedResponse<HelixStream>> GetStreams(string[] userIds, string after = null, string before = null, int first = 20)
+        public async Task<HelixPaginatedResponse<HelixStream>> GetStreams(string[] userIds, int first = 20, string language = null, string after = null, string before = null)
         {
             using var httpClient = GetHttpClient();
 
@@ -79,6 +86,46 @@ namespace Twitch.Net
 
             return await JsonSerializer.DeserializeAsync<HelixPaginatedResponse<HelixStream>>(responseStream);
         }
+        
+        /// <summary>
+        /// Get streams for specific usernames
+        /// </summary>
+        /// <param name="userLogins">Array of user login names. Limit: 100</param>
+        /// <param name="first">Amount of streams. Limit: 100</param>
+        /// <param name="language">Stream language. Limit: 100</param>
+        /// <param name="after">Cursor for pagination</param>
+        /// <param name="before">Cursor for pagination</param>
+        /// <returns><see cref="HelixPaginatedResponse{HelixStream}"/> with streams</returns>
+        public async Task<HelixPaginatedResponse<HelixStream>> GetStreams(string[] userLogins, int first = 20, string language = null, string after = null, string before = null)
+        {
+            using var httpClient = GetHttpClient();
 
+            var parameters = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("first", first.ToString())
+            };
+
+            parameters.AddRange(userLogins.Select(userLogin => new KeyValuePair<string, string>("user_login", userLogin)));
+
+            if (string.IsNullOrEmpty(after) == false)
+            {
+                parameters.Add(new KeyValuePair<string, string>("after", after));
+            }
+
+            if (string.IsNullOrEmpty(before) == false)
+            {
+                parameters.Add(new KeyValuePair<string, string>("before", before));
+            }
+
+            if (string.IsNullOrEmpty(language) == false)
+            {
+                parameters.Add(new KeyValuePair<string, string>("language", language));
+            }
+            
+            var responseStream = await httpClient.GetAsync(_getStreamsEndpoint, parameters);
+
+            return await JsonSerializer.DeserializeAsync<HelixPaginatedResponse<HelixStream>>(responseStream);
+        }
+        
     }
 }
