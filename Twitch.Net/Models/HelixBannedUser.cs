3a043c44bb70bcd1ba9e6201b877c1a5973d3b39
Fabian Vowie <crymaprivat@gmail.com>
2020-06-21 18:40:05 +0200

    Fix banned users deserialization

--- Twitch.Net/Models/HelixBannedUser.cs
+++ Twitch.Net/Models/HelixBannedUser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Xml;
 
 namespace Twitch.Net.Models
 {
@@ -13,7 +15,20 @@ namespace Twitch.Net.Models
         public string UserName { get; set; }
 
         [JsonPropertyName("expires_at")]
-        public DateTime ExpiresAt { get; set; }
+        public string ExpiresAtRaw { get; set; }
+
+        public DateTime? ExpiresAt
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ExpiresAtRaw))
+                {
+                    return null;
+                }
+
+                return XmlConvert.ToDateTime(ExpiresAtRaw, XmlDateTimeSerializationMode.Utc);
+            }
+        }
 
     }
 }
